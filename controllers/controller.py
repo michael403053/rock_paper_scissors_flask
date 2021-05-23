52fe73a6d0dffa7eb4dfd65c42c03e8883f4bca7
michael403053 <michael.christie5@hotmail.co.uk>
2021-05-23 14:42:00 +0100

    Easy, normal, hard mode with name input

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, request
 from app import app
 from models.rps import *
 
@@ -10,3 +10,47 @@ def index():
 def return_winner(player1_choice, player2_choice):
     result = win_finder(player1_choice, player2_choice)
     return render_template('result.html', result=result, player1_choice=player1_choice, player2_choice=player2_choice)
+    
+@app.route('/play')
+def play_page():
+    return render_template('play.html')
+
+@app.route('/play', methods=['POST'])
+def play():
+    player_choice = request.form['player_choice']
+    player_name = request.form['player_name']
+    result = play_rps(player_choice)
+    computer_choice = result[1]
+    winner = result[0]
+    return render_template("play.html", result=result, computer_choice=computer_choice, winner=winner, player_name=player_name)
+
+@app.route('/play/easy')
+def play_easy_page():
+    return render_template('easy.html')
+
+@app.route('/play/easy', methods=['POST'])
+def play_easy():
+    player_choice = request.form['player_choice']
+    result = play_rps_easy(player_choice)
+    player_name = request.form['player_name']
+    computer_choice = result[1]
+    winner = result[0]
+    return render_template("easy.html", result=result, computer_choice=computer_choice, winner=winner, player_name=player_name)
+
+@app.route('/play/hard')
+def play_hard_page():
+    return render_template('hard.html')
+
+@app.route('/play/hard', methods=['POST'])
+def play_hard():
+    player_choice = request.form['player_choice']
+    result = play_rps_hard(player_choice)
+    player_name = request.form['player_name']
+    computer_choice = result[1]
+    winner = result [0]
+    return render_template("hard.html", result=result, computer_choice=computer_choice, winner=winner, player_name=player_name)
+    
+
+
+
+
--- models/rps.py
+++ models/rps.py
@@ -2,7 +2,7 @@ def win_finder(player1_choice, player2_choice):
     p1win = "Player 1 is the winner!"
     p2win = "Player 2 is the winner!"
     tie = "Tie Match!"
-    if player1_choice == "rock" and player2_choice == "paper":
+    if player1_choice == "rock" and player2_choice  == "paper":
         return p2win
     if player1_choice == "rock" and player2_choice == "scissors":
         return p1win
@@ -20,3 +20,101 @@ def win_finder(player1_choice, player2_choice):
         return tie
     if player1_choice == "scissors" and player2_choice == "rock":
         return p2win
+
+import random
+
+def play_rps(player_choice):
+    pwin = "You are the winner!"
+    cwin = "Computer is the winner!"
+    tie = "Tie Match!"
+    rps_list = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(rps_list)
+
+    if player_choice == "rock" and computer_choice  == "paper":
+        return str(cwin), str(computer_choice)
+    if player_choice == "rock" and computer_choice == "scissors":
+        return str(pwin), str(computer_choice)
+    if player_choice == "rock" and computer_choice == "rock":
+        return tie, str(computer_choice)
+    if player_choice == "paper" and computer_choice == "paper":
+        return tie, str(computer_choice)
+    if player_choice == "paper" and computer_choice == "scissors":
+        return str(cwin), str(computer_choice)
+    if player_choice == "paper" and computer_choice == "rock":
+        return str(pwin), str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "paper":
+        return str(pwin), str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "scissors":
+        return tie, str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "rock":
+        return str(cwin), str(computer_choice)
+
+def play_rps_easy(player_choice):
+    pwin = "You are the winner!"
+    cwin = "Computer is the winner!"
+    tie = "Tie Match!"
+
+    if player_choice == "rock":
+        computer_choice = random.choice(["rock", "paper", "scissors", "scissors"])
+
+    if player_choice == "paper":
+        computer_choice = random.choice(["rock", "paper", "scissors", "rock"])
+
+    if player_choice == "scissors":
+        computer_choice = random.choice(["rock", "paper", "scissors", "paper"])
+ 
+
+    if player_choice == "rock" and computer_choice  == "paper":
+        return str(cwin), str(computer_choice)
+    if player_choice == "rock" and computer_choice == "scissors":
+        return str(pwin), str(computer_choice)
+    if player_choice == "rock" and computer_choice == "rock":
+        return tie, str(computer_choice)
+    if player_choice == "paper" and computer_choice == "paper":
+        return tie, str(computer_choice)
+    if player_choice == "paper" and computer_choice == "scissors":
+        return str(cwin), str(computer_choice)
+    if player_choice == "paper" and computer_choice == "rock":
+        return str(pwin), str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "paper":
+        return str(pwin), str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "scissors":
+        return tie, str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "rock":
+        return str(cwin), str(computer_choice)
+
+def play_rps_hard(player_choice):
+    pwin = "You are the winner!"
+    cwin = "Computer is the winner!"
+    tie = "Tie Match!"
+
+    if player_choice == "rock":
+        computer_choice = random.choice(["rock", "paper", "scissors", "paper"])
+
+    if player_choice == "paper":
+        computer_choice = random.choice(["rock", "paper", "scissors", "scissors"])
+
+    if player_choice == "scissors":
+        computer_choice = random.choice(["rock", "paper", "scissors", "rock"])
+ 
+
+    if player_choice == "rock" and computer_choice  == "paper":
+        return str(cwin), str(computer_choice)
+    if player_choice == "rock" and computer_choice == "scissors":
+        return str(pwin), str(computer_choice)
+    if player_choice == "rock" and computer_choice == "rock":
+        return tie, str(computer_choice)
+    if player_choice == "paper" and computer_choice == "paper":
+        return tie, str(computer_choice)
+    if player_choice == "paper" and computer_choice == "scissors":
+        return str(cwin), str(computer_choice)
+    if player_choice == "paper" and computer_choice == "rock":
+        return str(pwin), str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "paper":
+        return str(pwin), str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "scissors":
+        return tie, str(computer_choice)
+    if player_choice == "scissors" and computer_choice == "rock":
+        return str(cwin), str(computer_choice)
+
+
